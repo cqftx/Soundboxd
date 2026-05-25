63ad216f24663786836c057462a43f27084d6556
Andrzej Smaga <andrii.smaga@gmail.com>
2026-05-25 14:24:30 +0200

    feat: connect database and albums API
    connect prisma to PostgreSQL (Neon) and implement basic /albums endpoint.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const prisma = require("./prismaClient");
+
 const app = express();
 
 app.use(cors());
@@ -9,6 +11,17 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
+});
+
+console.log("REGISTERING /albums ROUTE");
+
+app.get("/albums", async (req, res) => {
+  try {
+    const albums = await prisma.album.findMany();
+    res.json(albums);
+  } catch (error) {
+    res.status(500).json({ error: "Error" });
+  }
 });
 
 const PORT = 5000;
